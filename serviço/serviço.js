22347d94d1d7f1925e5ba3151da6f2b6ae222cb7
Bruno-Scharf <159488457+Bruno-Scharf@users.noreply.github.com>
2024-11-28 16:13:05 -0400

    Add files via upload

---
+++ serviço/serviço.js
@@ -0,0 +1,13 @@
+import colecaoUf from "../dados/dados.js";
+
+export const buscarUfs = () => {
+    return colecaoUf;
+}
+export const buscarUfsPorNome = (nomeUf) => {
+    return colecaoUf.filter(uf => uf.nome.toLowerCase().includes(nomeUf.toLowerCase()));
+};
+
+export const buscarUfPorId = (id) => {
+    const idUf = partInt(id);
+    return colecaoUf.find(uf => uf.id == idUF);
+}
